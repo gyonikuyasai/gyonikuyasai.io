aaaaa690d319e04d7c37436a4513c0e761f6482d
unknown <sakananikuyasai2003@gmail.com>
2020-03-04 21:16:14 -0700

    hoge

--- resources/scripts/common.js
+++ resources/scripts/common.js
@@ -1,21 +1,42 @@
-// Make HTML Table From 2D Array `data` to the Element whose ID is `tableId`
-// `data` assume the following 2D array:
-//  - row    0: heading
-//  - column 0: label
-//  - column 1: url
-function makeTable(data, tableId) {
+// Create `a` tag element then return it
+function createLink(text, url) {
+  var anchor = document.createElement('a');
+  anchor.textContent = text;
+  anchor.innerText = text;
+  anchor.href = url;
+  anchor.target = "_blank";
+  return anchor;
+}
+
+// Convert CSV to 2D array then return the 2D array
+//  - `str`: responseText of XMLHttpRequest to the URL of CSV
+function convertCSVtoArray(str){
+  var result = [];
+  var tmp = str.split("\n");
+  for(var i = 0; i < tmp.length; i++) {
+    result[i] = tmp[i].split(',');
+  }
+  return result;
+}
+
+// Render HTML table from 2D Array `data` to the element whose ID is `tableId`
+//   `data` assume the following 2D array:
+//     - row    0: heading
+//     - column 0: label
+//     - column 1: url
+function renderTable(data, tableId) {
   var table = document.createElement("table");
   var rows = [];
-  // Loop for rows of data
+  // Loop for row of `data`
   for (var i = 0; i < data.length; i++) {
     rows.push(table.insertRow(-1));
     cell = rows[i].insertCell(-1);
-    // First row is heading
+    // row 0 is heading
     if (i == 0) {
       // Change the color of the cell of heading
       cell.style.backgroundColor = "#55acee";
       cell.style.color = "white";
-      // Append label to the cell using label in column 0
+      // Append label to the cell
       cell.appendChild(document.createTextNode(data[i][0]));
     } else {
       // Append link to the cell
@@ -26,25 +47,4 @@ function makeTable(data, tableId) {
   }
   table.align = "center";
   document.getElementById(tableId).appendChild(table);
-}
-
-// Convert CSV to 2D array then return the 2D array
-//  - str: responseText of XMLHttpRequest to the URL of CSV
-function convertCSVtoArray(str){
-  var result = [];
-  var tmp = str.split("\n");
-  for(var i = 0; i < tmp.length; i++) {
-    result[i] = tmp[i].split(',');
-  }
-  return result;
-}
-
-// Create `a` tag element from `text` and `url`
-function createLink(text, url) {
-  var anchor = document.createElement('a');
-  anchor.textContent = text;
-  anchor.innerText = text;
-  anchor.href = url;
-  anchor.target = "_blank";
-  return anchor;
 }
--- resources/scripts/roulette.js
+++ resources/scripts/roulette.js
@@ -1,27 +1,28 @@
+// Variable definition
 var roulette;
 var idx;
 var lunchSpotData = [];
 
-// Make lunch table when this page loaded
+// Render lunch spot table when this page loaded
 window.onload = function() {
-  makeLunchSpotTable();
+  renderLunchSpotTable();
 } 
 
-// Make lunch table from CSV
-function makeLunchSpotTable() {
+// Render lunch spot table from CSV
+function renderLunchSpotTable() {
   var url = "https://gyonikuyasai.github.io/gyonikuyasai.io/resources/csv/lunchSpot.csv";
   var req = new XMLHttpRequest;
   req.open("get", url);
   req.send(null);
   req.onload = function() {
     lunchSpotData = convertCSVtoArray(req.responseText);
-    makeTable(lunchSpotData, "lunchSpotTable");
+    renderTable(lunchSpotData, "lunchSpotTable");
   }
 }
 
 // Start roulette
 function start() {
-  roulette = setInterval(generateRoulette, 100);
+  roulette = setInterval(renderRoulette, 100);
   // Reset `lunchSpot`
   document.getElementById("lunchSpot").innerHTML = "本日の昼食は?";
 }
@@ -30,15 +31,15 @@ function start() {
 function stop() {
   if(roulette) {
     clearInterval(roulette);
-    // Generate `lunchSpot`
+    // Render `lunchSpot`
     document.getElementById("lunchSpot").innerHTML = lunchSpotData[idx][0];
   }
 }
 
-// Generate roulette with random value
-function generateRoulette() {
+// Render roulette with random value
+function renderRoulette() {
   // Generate random value from 1 to (`lunchSpotData`.length - 1). Exclude the value 0 (row:0 is heading)
   idx = Math.floor( Math.random() * (lunchSpotData.length - 1)) + 1;
-  // Generate roulette
+  // Render roulette
   document.getElementById("roulette").innerHTML = idx;
 }
